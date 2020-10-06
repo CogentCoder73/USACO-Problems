8789ab320cbf3676e595979358325fe0ef032492
CogentCoder73 <59863688+CogentCoder73@users.noreply.github.com>
2020-10-05 17:38:05 -0700

    Update and rename mixingmilk.cpp to USACO Bronze Problems/2018/December/mixingmilk.cpp

--- USACO Bronze Problems/2018/December/mixingmilk.cpp
+++ USACO Bronze Problems/2018/December/mixingmilk.cpp
@@ -1,14 +1,3 @@
-/*
-ID: mathboy6
-TASK: mixmilk
-LANG: C++
-*/
-
-//#pragma GCC optimize ("O3")
-//#pragma GCC target ("sse4")
-
-#define _CRT_SECURE_NO_WARNINGS
-/*
 #include <bits/stdc++.h>
 #include <iostream>
 #include <fstream>
@@ -53,5 +42,3 @@ int main() {
     cout << cow3.second << endl;
     return 0;
 }
-
-*/
