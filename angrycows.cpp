43259fb882abf7fd72757c55f9ba19ed94f7f66e
CogentCoder73 <59863688+CogentCoder73@users.noreply.github.com>
2020-10-05 17:03:17 -0700

    Update angrycows.cpp

--- angrycows.cpp
+++ angrycows.cpp
@@ -1,25 +1,3 @@
-/*
-ID: mathboy6
-TASK: test
-LANG: C++
-*/
-/*
-//#pragma GCC optimize ("O3")
-//#pragma GCC target ("sse4")
-
-#define _CRT_SECURE_NO_WARNINGS
-
-#include <bits/stdc++.h>
-#include <iostream>
-#include <fstreamTASK: test
-LANG: C++
-*/
-/*
-//#pragma GCC optimize ("O3")
-//#pragma GCC target ("sse4")
-
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <bits/stdc++.h>
 #include <iostream>
 #include <fstream>
@@ -118,4 +96,4 @@ int main() {
     //}
     cout << *max_element(nums.begin(), nums.end()) << endl;
     return 0;
-}*/
+}
